b8c6ed57a1d40b5eb706b12676ca85f1aea84784
k.shevchenko94 <k.shevchenko94@gmail.com>
2017-06-21 22:20:03 +0300

    доработка (reverted from commit 541b23f248e1124e0f769dd559eaa7631182da9a)

--- modules/user/views/tehpoddershka/index.php
+++ modules/user/views/tehpoddershka/index.php
@@ -1,16 +1,3 @@
 <?php
 $this->title = 'Техподдержка';
 ?>
-<div class="row">
-	<div class="col-md-6">
-		<h2><?=$this->title?></h2>
-	</div>
-	<div class="col-md-6">
-		<a href="#" class="btn btn_tegpod pull-right">Ответы на часто задаваемые вопросы</a>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-12">
-		<h4 style="color:#8492af;">Укажите тему письма:</h4>
-	</div>
-</div>
